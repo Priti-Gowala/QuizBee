b6cc10e19f776b8c787c5d8cf2ad341f3f8205d0
unknown <pritigowala7@gmail.com>
2020-08-01 14:04:10 +0530

    Updates

---
+++ precache-manifest.9369f0f4248ba838231365e94a9adfed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb994d86686897d054fffbecb2cefae",
+    "url": "/QuizBee/index.html"
+  },
+  {
+    "revision": "b6eb5bdf3ee782f0e07f",
+    "url": "/QuizBee/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "db2f999bf277dff8d36d",
+    "url": "/QuizBee/static/js/2.ac3dd050.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QuizBee/static/js/2.ac3dd050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6eb5bdf3ee782f0e07f",
+    "url": "/QuizBee/static/js/main.3c45b1ba.chunk.js"
+  },
+  {
+    "revision": "0de30a0ed5df0dc0c94d",
+    "url": "/QuizBee/static/js/runtime-main.b39360ae.js"
+  }
+]);
